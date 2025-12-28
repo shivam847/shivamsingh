0d5dc24ce7fe93aebfeec1e1e416cb74f3c567eb
shivam847 <shivsisodiya3172000@gmail.com>
2025-12-28 15:06:19 +0530

    update navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,8 @@
 
+import { Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 const navLinks = [
     { href: "#about", label: "About" },
     { href: "#projects", label: "Projects" },
@@ -6,6 +10,9 @@ const navLinks = [
 ];
 
 export const Navbar = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -13,14 +20,33 @@ export const Navbar = () => {
                     SS<span className="text-primary">.</span>
                 </a>
 
-                <div className="flex items-center gap-1">
+                <div className="hidden md:flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
-                        {navLinks.map((link,index) => (
+                        {navLinks.map((link, index) => (
                             <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">{link.label}</a>
                         ))}
                     </div>
                 </div>
+                <div className="hidden md:block">
+                    <Button size="sm">Contact me</Button>
+                </div>
+                {/* mobileMenuButton */}
+
+                <button className="md:hidden p-2 text-foreground cursor-pointer" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
             </nav>
+
+            {/* mobileMenu */}
+
+            {isMobileMenuOpen && (<div className="md:hidden glass-strong animate-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                    {navLinks.map((link, index) => (
+                        <a href={link.href} key={index} className="text-lg text-muted-foreground hover:text-foreground">{link.label}</a>
+                    ))}
+                    <Button>Contact me</Button>
+                </div>
+            </div>)}
         </header>
     );
 };
